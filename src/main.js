d0bc1e94718c4063514b108c7f0c56b1a0907bc3
Shuhei Nishimura <nishimura@digitalcube.jp>
2016-10-24 08:43:09 +0900

    script move footer

--- src/main.js
+++ src/main.js
@@ -1,25 +1,3 @@
-//const riot = require("riot");
-//
-//require("./components/app.tag");
-//require("./components/main-header.tag");
-//require("./components/raw.tag");
-//require("./components/post.tag");
-
-//fetch("http://api.wp-app.org/wp-json/wp/v2/posts", {
-//	method: "GET",
-//	mode: 'cors'
-//}).then( (data) => {
-//	return data.json();
-//}).then( (data) => {
-//	console.log(data);
-//	riot.mount("app", {
-//		posts: data
-//	});
-//}).catch( (err) => {
-//	alert(err);
-//});
-
-
 //var flag = true;
 //this.click = function() {
   //console.log('now click');
