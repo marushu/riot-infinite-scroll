a81d0a7bf191a4834edbf98cc6ebe685a75108b0
Shuhei Nishimura <nishimura@digitalcube.jp>
2016-10-25 20:42:03 +0900

    modify per_page page param

--- src/main.js
+++ src/main.js
@@ -1,24 +1,28 @@
 flag = false;
-var counter = 1
-get_remote_datas( 'app', false, counter );
+var per_page = 5
+var paged = 1
+get_remote_datas( 'app', false, per_page, paged );
 
 var nextArray = this.items = []
 this.click = function( e ) {
 
-  counter++
-  var test = get_remote_datas( 'nextload', true, counter )
+  paged++
+  console.log( paged )
+  var test = get_remote_datas( 'nextload', true, per_page, paged )
 
 }
 
-function get_remote_datas( target, check, paged ) {
+function get_remote_datas( target, check, per_page, paged ) {
+
+  console.log( per_page )
 
   var baseUrl = 'https://private.hibou-web.com/wp-json/wp/v2/posts'
-  var posts_per_page = 2
-  var pageNum = 1
   var param = '?per_page='
+  var per_page = per_page
+  var paged_str = '&page='
   var paged = paged
-  var requestUrl = baseUrl + param + paged
-  //console.log( requestUrl )
+  var requestUrl = baseUrl + param + per_page + paged_str + paged
+  console.log( requestUrl )
 
   $.ajax({
     url      : requestUrl,
@@ -31,7 +35,7 @@ function get_remote_datas( target, check, paged ) {
       posts: response
     });
 
-    console.log( this.items )
+    //console.log( this.items )
 
   }).fail(function( response ) {
 
